5d492628e8710572805007527cdfe85a7a75e1f1
Nicew <805235292@qq.com>
2017-08-03 16:39:21 +0800

    增加后台自动更新天气功能。

--- app/src/main/java/com/nicew/coolweather/WeatherActivity.java
+++ app/src/main/java/com/nicew/coolweather/WeatherActivity.java
@@ -1,14 +1,15 @@
 package com.nicew.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
+import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.nicew.coolweather.gson.Forecast;
 import com.nicew.coolweather.gson.Weather;
+import com.nicew.coolweather.service.AutoUpdateService;
 import com.nicew.coolweather.util.HttpUtil;
 import com.nicew.coolweather.util.Utility;
 
@@ -221,5 +223,7 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 }
